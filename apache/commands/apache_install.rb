0a5ea6d9e3fbe82134c0616df938a0b7b70f4674
Philipp T <philipp@virtualop.org>
2011-10-31 00:56:38 +0100

    rather substantially refactored plugin loading

--- apache/commands/apache_install.rb
+++ apache/commands/apache_install.rb
@@ -1,10 +1,11 @@
 description "installs an apache httpd server"
 
 param :machine
+param :service_root
 
 on_machine do |machine, params|
   #machine.install_rpm_package("name" => "httpd")
-  
+    
   process_template(:httpd_conf, machine, "/etc/httpd/conf/httpd.conf", binding())
   
   machine.mkdir("dir_name" => '/etc/httpd/conf.d.generated')
