2e41993f9708d431cf1cd5895bbd259706f9ca74
Philipp T <philipp@virtualop.org>
2011-11-13 01:54:22 +0100

    jenkins should be installable on debian as well now, some parsing in list_configured_vhosts

--- apache/commands/apache_install.rb
+++ apache/commands/apache_install.rb
@@ -4,7 +4,6 @@ param :machine
 param :service_root
 
 on_machine do |machine, params|
-  #machine.install_rpm_package("name" => "httpd")
   machine.rm("file_name" => "/etc/httpd/conf.d/welcome.conf") if machine.file_exists("file_name" => "/etc/httpd/conf.d/welcome.conf")
     
   process_template(:httpd_conf, machine, "/etc/httpd/conf/httpd.conf", binding()) 
--- apache/services/apache.rb
+++ apache/services/apache.rb
@@ -1,3 +1,12 @@
 port 80
 process_regex /httpd/
 unix_service :httpd
+
+on_install do |machine, service_root, params|
+  machine.rm("file_name" => "/etc/httpd/conf.d/welcome.conf") if machine.file_exists("file_name" => "/etc/httpd/conf.d/welcome.conf")
+    
+  process_template(:httpd_conf, machine, "/etc/httpd/conf/httpd.conf", binding()) 
+  
+  machine.mkdir("dir_name" => '/etc/httpd/conf.d.generated')
+end
+
