80f6405a0d2bb39dbbdb55b5ca53f492e1458721
Philipp T <philipp@virtualop.org>
2011-11-13 02:24:00 +0100

    included vhost config in /etc/httpd/conf.d into list_configured_vhosts

--- apache/commands/list_configured_vhosts.rb
+++ apache/commands/list_configured_vhosts.rb
@@ -2,28 +2,32 @@ description 'lists the virtual hosts that have been configured on this apache'
 
 param :machine
 
-add_columns [ :domain, :target_ip ]
+add_columns [ :domain, :target_ip, :document_root ]
 
 mark_as_read_only
 
 on_machine do |machine, params|
   result = []
-  directory = "/etc/httpd/conf.d.generated/"
-  machine.list_files("directory" => directory).each do |file_name|
-    h = {}
-    machine.ssh_and_check_result("command" => "cat #{directory}/#{file_name}").split("\n").each do |line|
-      if matched = /ServerName\s+(.+)/.match(line)
-        h["domain"] = matched.captures.first
-      elsif matched = /ProxyPass\s+\/\s+(.+)/.match(line)
-        h["target"] = matched.captures.first
-        if inner_match = /^http\:\/\/(.+)\/$/.match(h["target"])
-          h["target_ip"] = inner_match.captures.first
-        end
-      elsif matched = /DocumentRoot\s+(.+)/.match(line)
-        h["document_root"] = matched.captures.first        
-      end        
+  [ "/etc/httpd/conf.d.generated/", "/etc/httpd/conf.d/" ].each do |directory|
+    machine.list_files("directory" => directory).each do |file_name|
+      
+      next unless /\.conf$/.match(file_name.strip)
+      
+      h = {}
+      machine.ssh_and_check_result("command" => "cat #{directory}/#{file_name}").split("\n").each do |line|
+        if matched = /ServerName\s+(.+)/.match(line)
+          h["domain"] = matched.captures.first
+        elsif matched = /ProxyPass\s+\/\s+(.+)/.match(line)
+          h["target"] = matched.captures.first
+          if inner_match = /^http\:\/\/(.+)\/$/.match(h["target"])
+            h["target_ip"] = inner_match.captures.first
+          end
+        elsif matched = /DocumentRoot\s+(.+)/.match(line)
+          h["document_root"] = matched.captures.first        
+        end        
+      end
+      result << h
     end
-    result << h
   end
   result
 end
