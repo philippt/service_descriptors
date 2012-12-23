3976f27a3f17aa996bff8d3d99b972c21e206138
root <root@vopdev.(none)>
2012-12-23 16:56:58 +0100

    made post launch parameterizable in trigger_build

--- jenkins/commands/trigger_build.rb
+++ jenkins/commands/trigger_build.rb
@@ -1,6 +1,7 @@
 description "asks jenkins (nicely) to execute a build for the specified jenkins job"
 
 param! :jenkins_job
+param "wait", "if 'true', will wait for the triggered build to be completed", :lookup_method => lambda { %w|true false| }, :default_value => "false"
 
 execute do |params|
   $logger.info "triggering build for #{params["jenkins_job"]}"
@@ -26,23 +27,26 @@ execute do |params|
     "url" => "#{config_string('jenkins_url')}/job/#{job_name}/build"
   )
   
-  sleep config_string('post_launch_wait_secs', "5").to_i
-  
-  @op.without_cache do
-    new_builds = @op.list_builds_for_job(
-      "jenkins_job" => params["jenkins_job"]
-    )
+  if params["wait"] == "true"
+    sleep config_string('post_launch_wait_secs', "5").to_i
     
-    last_build_now = new_builds.sort do |b,a|
-      a["number"] <=> b["number"]
-    end.first
-    
-    if last_build_so_far != nil
-      raise "did not find a build newer than ##{last_build_now}" unless (last_build_now["number"] > last_build_so_far["number"])
+    @op.without_cache do
+      new_builds = @op.list_builds_for_job(
+        "jenkins_job" => params["jenkins_job"]
+      )
+      
+      last_build_now = new_builds.sort do |b,a|
+        a["number"] <=> b["number"]
+      end.first
+      
+      if last_build_so_far != nil
+        raise "did not find a build newer than ##{last_build_now}" unless (last_build_now["number"] > last_build_so_far["number"])
+      end
+      
+      result = last_build_now["number"]
     end
-    
-    result = last_build_now["number"]
   end
+  
   
   result
 end
