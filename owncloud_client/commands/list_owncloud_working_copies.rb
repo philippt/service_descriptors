24bb55df84de6bdc074b504b028332567da49287
root <root@dev_vop.(none)>
2013-02-20 13:29:22 +0100

    centos_ldap, owncloud

--- owncloud_client/commands/list_owncloud_working_copies.rb
+++ owncloud_client/commands/list_owncloud_working_copies.rb
@@ -9,11 +9,15 @@ result_as :list_working_copies
 
 on_machine do |machine, params|
   project_path = config_string('owncloud_path') + '/projects'
-  machine.list_files("directory" => project_path).map do |path|
-    {
+  result = []
+  
+  machine.list_files("directory" => project_path).each do |path|
+    result << {
       "path" => project_path + '/' + path,
       "type" => "owncloud",
       "name" => path.split("/").last
     }
-  end
+  end if machine.file_exists("file_name" => project_path)
+  
+  result
 end
--- owncloud_server/commands/owncloud_server_install.rb
+++ owncloud_server/commands/owncloud_server_install.rb
@@ -4,5 +4,6 @@ param! "domain"
 on_machine do |machine, params|
   # TODO move into static_html, :document_root => 'foo' ?
   machine.add_static_vhost("server_name" => params["domain"], "document_root" => "/var/www/html/owncloud")
+  machine.restart_service("service" => "apache")
   machine.configure_reverse_proxy("domain" => params["domain"])
 end
