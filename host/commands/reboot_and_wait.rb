a77b98b78afae548a5f27a792a82d2d14f33580a
root <root@dev_vop.(none)>
2013-02-24 14:28:22 +0100

    bugfix for wait_until in reboot_and_wait, .vop template for service descriptor plugins

--- host/commands/reboot_and_wait.rb
+++ host/commands/reboot_and_wait.rb
@@ -10,20 +10,17 @@ execute do |params|
   @op.flush_cache
   sleep 15
   
-  @op.wait_until(
-    "interval" => 5, "timeout" => 120, 
-    "error_text" => "could not find a running machine with name '#{params["machine"]}'",
-    "condition" => lambda do
-      result = false
-      begin
-        @op.with_machine(params["machine"]) do |new_connection|
-          new_connection.hostname
-        end      
-        result = true
-      rescue Exception => e
-        $logger.info("got an exception while trying to connect to machine : #{e.message}")
-      end
-      result
+  @op.wait_until("interval" => 5, "timeout" => 120, 
+    "error_text" => "could not find a running machine with name '#{params["machine"]}'") do
+    result = false
+    begin
+      @op.with_machine(params["machine"]) do |new_connection|
+        new_connection.hostname
+      end      
+      result = true
+    rescue Exception => e
+      $logger.info("got an exception while trying to connect to machine : #{e.message}")
     end
-  )
+    result
+  end
 end
