f9ac95651bf7fc3a57e80559563d2cb9c7ece464
Philipp T <philipp@virtualop.org>
2013-07-13 15:48:50 +0200

    rvm: get stable and --verify-downloads

--- rvm/commands/rvm_install.rb
+++ rvm/commands/rvm_install.rb
@@ -2,15 +2,19 @@ param :machine
 param! "user", "name of the user account in whose context rvm should be installed"
 param "ruby_version", "a version of ruby to install using the newly installed rvm"
 
+# TODO needs_sudo
+
 on_machine do |m, params|
   m.as_user("user_name" => params["user"]) do |machine|
     tmp_dir = "/home/#{params["user"]}/tmp"
+    machine.mkdir tmp_dir
     machine.ssh("cd #{tmp_dir} && curl -k -L https://get.rvm.io > rvm_io.sh && chmod +x rvm_io.sh")
     machine.ssh("cd #{tmp_dir} && ./rvm_io.sh")
     
+    machine.rvm_ssh("rvm get stable")
+    
     if params.has_key?("ruby_version")
-      # TODO #fail this does not work in the correct user context
-      machine.rvm_ssh("rvm install #{params["ruby_version"]}")
+      machine.rvm_ssh("rvm install #{params["ruby_version"]} --verify-downloads 1")
     end
   end
 end
