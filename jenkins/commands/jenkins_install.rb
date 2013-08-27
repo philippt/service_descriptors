161f1751f7fc2c7ac719f7eb8437056ec16466e5
Philipp T <philipp@virtualop.org>
2013-08-27 10:48:21 +0200

    user param for jenkins_install, adjustments for rabbitmq and dropbox excludes

--- jenkins/commands/jenkins_install.rb
+++ jenkins/commands/jenkins_install.rb
@@ -1,10 +1,22 @@
 param :machine
 param "domain", "the domain at which jenkins should be available"
 
+param "jenkins_user", "name of the system account that the jenkins process should run as", :default_value => "jenkins"
+
 as_root do |machine, params|
   @op.flush_cache
-  if machine.file_exists("file_name" => "/etc/sysconfig/jenkins")
-    machine.ssh("command" => "sed -i -e 's/JENKINS_USER=\"jenkins\"/JENKINS_USER=\"root\"/g' /etc/sysconfig/jenkins")
-  end    
-  machine.start_unix_service("name" => "jenkins")
+  
+  if machine.file_exists("file_name" => "/etc/sysconfig/jenkins")    
+    machine.ssh("sed -i -e 's/JENKINS_USER=\".*\"/JENKINS_USER=\"#{params["jenkins_user"]}\"/g' /etc/sysconfig/jenkins")
+  end
+  
+  %w|/var/lib/jenkins/ /var/cache/jenkins/ /var/log/jenkins/|.each do |dir_name|
+    if machine.file_exists dir_name
+      machine.chown(
+        "file_name" => dir_name, 
+        "ownership" => params["jenkins_user"]
+      )
+    end
+  end
+  #machine.start_unix_service("name" => "jenkins")
 end
