23b7feac19052ba9848b47d8591d5f5115de7246
Philipp T <philipp@virtualop.org>
2013-10-01 14:31:09 +0200

    rvm_version, tomcat dependency for java7

--- rvm/commands/rvm_install.rb
+++ rvm/commands/rvm_install.rb
@@ -1,6 +1,7 @@
 param :machine
 param! "user", "name of the user account in whose context rvm should be installed"
 param "ruby_version", "a version of ruby to install using the newly installed rvm"
+param "rvm_version", "a specific version of RVM that should be installed"
 
 # TODO needs_sudo
 
@@ -9,7 +10,9 @@ on_machine do |m, params|
     tmp_dir = "/home/#{params["user"]}/tmp"
     machine.mkdir tmp_dir
     machine.ssh("cd #{tmp_dir} && curl -k -L https://get.rvm.io > rvm_io.sh && chmod +x rvm_io.sh")
-    machine.ssh("cd #{tmp_dir} && ./rvm_io.sh")
+    
+    rvm_version = params.has_key?("rvm_version") ? params["rvm_version"] : ''
+    machine.ssh("cd #{tmp_dir} && ./rvm_io.sh #{rvm_version}")
     
     machine.rvm_ssh("rvm get stable")
     
