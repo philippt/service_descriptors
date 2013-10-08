4ce54d8f5398801f130a1a4f0c5e08f0df08519a
Philipp T <philipp@virtualop.org>
2013-10-08 15:34:15 +0200

    datarepo_dir through service_details[repo_dir] instead of vhosts

--- datarepo/commands/datarepo_dir.rb
+++ datarepo/commands/datarepo_dir.rb
@@ -2,6 +2,14 @@ param :machine
 
 on_machine do |machine, params|
   details = machine.service_details("service" => "datarepo")
-  vhost = machine.list_configured_vhosts.select { |x| x["domain"] == details["domain"] }.first
-  vhost["document_root"]
+  
+  if details.has_key?("extra_params") && details["extra_params"].has_key?("repo_dir")
+    result = details["extra_params"]["repo_dir"]
+  end
+  
+  if result.is_a?(Array)
+    result = result.first
+  end
+  
+  result
 end
