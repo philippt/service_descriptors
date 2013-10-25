92801b0e412f55a5e06e85780a26461f4170547a
Philipp T <philipp@virtualop.org>
2013-10-25 19:50:03 +0200

    list_queues

---
+++ rabbitmq/commands/list_queues.rb
@@ -0,0 +1,9 @@
+param :machine
+
+add_columns [ :name, :messages_ready ]
+
+on_machine do |machine, params|
+  url = machine.service_details("service" => "rabbitmq/webinterface")["domain"]
+  result = JSON.parse(@op.http_get("url" => "http://#{url}/api/queues", "user" => "guest", "password" => "guest"))
+  result
+end
