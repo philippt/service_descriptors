ca0b399651a6e4852b8e46ab8fce978fcc4ba8ba
Philipp T <philipp@virtualop.org>
2013-10-27 17:27:32 +0100

    fix for restore_dump

--- my_sql/commands/restore_dump.rb
+++ my_sql/commands/restore_dump.rb
@@ -1,6 +1,6 @@
 description "restores the local database(s) from a dump. old data gets lost while doing so."
 
-add_columns [ :name, :date, :host ]
+display_type :list
 
 param :machine
 param! :dump, "the dump from which we're going to restore"
@@ -10,5 +10,5 @@ on_machine do |machine, params|
   dump_name = params["dump_name"]
   $logger.info("restoring local databases from dump " + dump_name)
 
-  machine.restore_dump_from_file(params.merge("file_name" => dump_dir + '/' +  dump_name))
+  machine.restore_dump_from_file(params.merge("file_name" => dump_dir + '/' +  dump_name + '.tgz'))
 end
