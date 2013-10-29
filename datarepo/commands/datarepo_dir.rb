796cceacfa1e845fc03a3e89152c528f6e5f7079
Philipp T <philipp@virtualop.org>
2013-10-29 11:50:47 +0100

    default result for datarepo_dir

--- datarepo/commands/datarepo_dir.rb
+++ datarepo/commands/datarepo_dir.rb
@@ -1,15 +1,16 @@
 param :machine
 
 on_machine do |machine, params|
-  details = machine.service_details("service" => "datarepo")
+  details = machine.service_details("service" => "datarepo/datarepo")
   
   if details.has_key?("extra_params") && details["extra_params"].has_key?("repo_dir")
     result = details["extra_params"]["repo_dir"]
+    if result.is_a?(Array)
+      result = result.first
+    end
   end
   
-  if result.is_a?(Array)
-    result = result.first
-  end
+  result ||= '/var/www/html' # TODO this is already defined as default value for datarepo_install.repo_dir
   
   result
 end
