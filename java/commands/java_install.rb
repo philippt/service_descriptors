f476dd5d2f8bfc5b10a44fb1230f3d650424ea5a
Philipp T <philipp@virtualop.org>
2013-11-26 13:25:57 +0100

    java_manual_install, support for java6 on SLES 11 SP3

---
+++ java/commands/java_install.rb
@@ -0,0 +1,5 @@
+param :machine
+
+on_machine do |machine, params|
+  
+end
---
+++ java/commands/java_manual_install.rb
@@ -0,0 +1,22 @@
+param :machine
+
+on_machine do |machine, params|
+  search_dir = "#{@op.home('machine' => 'localhost')}/tmp"
+  jres = @op.local_java_packages
+  raise "no JREs found locally - searched on localhost in #{search_dir}" if jres.size == 0
+  jre = jres.first
+        
+  target_dir = '/usr/local/lib/'
+  machine.upload_file('local_file' => "#{search_dir}/#{jre}", 'target_file' => "#{machine.home}/tmp/#{jre}")
+  machine.explode('tar_name' => "#{machine.home}/tmp/#{jre}", 'working_dir' => target_dir)
+  
+  installed = machine.list_files("#{target_dir}/jdk*")
+  raise "could not find installed JDK in #{target_dir} after extracting - searched in #{target_dir}" if installed.size == 0
+  path = installed.first
+  
+  unless machine.file_exists("#{target_dir}/java")
+    machine.ssh("ln -s #{target_dir}/#{path} #{target_dir}/java")
+  end  
+  
+  process_local_template(:manual_java, machine, '/etc/profile.d/manual_java.sh', binding())
+end
