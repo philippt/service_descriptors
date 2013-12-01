f39794a69846dc834b83db3f987b23852be759af
Philipp T <philipp@virtualop.org>
2013-12-01 11:37:12 +0100

    twist for process_static_html

--- apache/commands/process_static_html.rb
+++ apache/commands/process_static_html.rb
@@ -13,10 +13,19 @@ on_machine do |machine, params|
       raise "static_html configuration found for service #{service["name"]}, but no domain parameter is present. don't know where to publish static html pages"
     end
     
+    
     domain = params["extra_params"]["domain"]
     machine.install_canned_service("service" => "apache/apache")
 
-    machine.add_static_vhost("server_name" => domain, "document_root" => service["service_root"])
+    vhost_options = {
+      "server_name" => domain, 
+      "document_root" => service["service_root"]
+    }
+    options = service["static_html"]
+    if options.is_a?(Hash) && options.has_key?('twist')
+      vhost_options['twist'] = options['twist']
+    end 
+    machine.add_static_vhost(vhost_options)
     machine.allow_access_for_apache("file_name" => service["service_root"])
     machine.restart_service 'apache/apache'
     
