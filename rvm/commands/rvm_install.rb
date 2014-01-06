d3ea3d81de1f02b7c5a0f62c7974f45a6eb9f3df
Philipp T <philipp@virtualop.org>
2014-01-06 12:46:17 +0100

    installing rvm through rpm from packages.dev.virtualop.org

--- rvm/commands/rvm_install.rb
+++ rvm/commands/rvm_install.rb
@@ -1,15 +1,17 @@
 param :machine
-param! "user", "name of the user account in whose context rvm should be installed"
+param! "user", "name of the user account in whose context rvm should be used"
 param "ruby_version", "a version of ruby to install using the newly installed rvm"
 
-# TODO needs_sudo
-
-on_machine do |m, params|
-  m.as_user("user_name" => params["user"]) do |machine|
-    machine.ssh("rvm get stable")
-    
-    if params.has_key?("ruby_version")
-      machine.ssh("rvm install #{params["ruby_version"]} --verify-downloads 1")
-    end
-  end
+as_root do |machine, params|
+  #m.as_user(params['user']) do |machine|
+  #m.as_user('root') do |machine|
+  machine.add_system_user(params['user']) unless machine.list_system_users.map { |x| x['name'] }.include? params['user']
+  machine.ssh("usermod -a -G rvm #{params['user']}")
+  
+  #machine.ssh("rvm get head --auto-dotfiles")
+  
+  #if params.has_key?("ruby_version")
+  #  machine.ssh("rvm install #{params["ruby_version"]} --verify-downloads 1")
+  #end
+  #end
 end
