ca6277dcfda99df6c4092eae859d33af14cab85d
Philipp T <philipp@virtualop.org>
2014-01-08 15:51:55 +0100

    java_manual_install

--- java/commands/java_manual_install.rb
+++ java/commands/java_manual_install.rb
@@ -10,12 +10,13 @@ on_machine do |machine, params|
   machine.upload_file('local_file' => "#{search_dir}/#{jre}", 'target_file' => "#{machine.home}/tmp/#{jre}")
   machine.explode('tar_name' => "#{machine.home}/tmp/#{jre}", 'working_dir' => target_dir)
   
-  installed = machine.list_files("#{target_dir}/jdk*")
+  installed = machine.find('path' => target_dir, 'type' => 'd', 'maxdepth' => 1, 'name' => 'jdk*')
+  #installed = machine.list_files("#{target_dir}/jdk*")
   raise "could not find installed JDK in #{target_dir} after extracting - searched in #{target_dir}" if installed.size == 0
   path = installed.first
   
   unless machine.file_exists("#{target_dir}/java")
-    machine.ssh("ln -s #{target_dir}/#{path} #{target_dir}/java")
+    machine.ssh("ln -s #{path} #{target_dir}/java")
   end  
   
   process_local_template(:manual_java, machine, '/etc/profile.d/manual_java.sh', binding())
